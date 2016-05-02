f5838918d743fc0c161c279930ac5f373061fdd7
Tom Burdick <thomas.burdick@gmail.com>
2016-05-02 10:38:34 -0500

    Remove extraneous header file comment

--- CommuteStream/CSCustomBanner.h
+++ CommuteStream/CSCustomBanner.h
@@ -1,11 +1,3 @@
-//
-//  CSCustomBanner.h
-//  CommuteStream
-//
-//  Created by David Rogers on 5/3/14.
-//  Copyright (c) 2014 CommuteStream. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import "GADCustomEventBanner.h"
 #import "GADCustomEventBannerDelegate.h"
