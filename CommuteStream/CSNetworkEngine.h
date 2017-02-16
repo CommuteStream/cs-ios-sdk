bba3691fadfd54132625e81f5e9f45d38bbc9ed4
Tom Burdick <thomas.burdick@gmail.com>
2017-02-16 10:57:56 -0600

    chore: remove unused copyright commentary

--- CommuteStream/CSNetworkEngine.h
+++ CommuteStream/CSNetworkEngine.h
@@ -1,11 +1,3 @@
-///Users/tburdick/src/ios-sdk/CommuteStream
-//  CSNetworkEngine.h
-//  CommuteStream
-//
-//  Created by David Rogers on 5/3/14.
-//  Copyright (c) 2014 CommuteStream. All rights reserved.
-//
-
 #import "MKNetworkKit.h"
 #import "Csmessages.pbobjc.h"
 
--- CommuteStream/CSNetworkEngine.m
+++ CommuteStream/CSNetworkEngine.m
@@ -1,11 +1,3 @@
-//
-//  CSNetworkEngine.m
-//  CommuteStream
-//
-//  Created by David Rogers on 5/3/14.
-//  Copyright (c) 2014 CommuteStream. All rights reserved.
-//
-
 #import "CSNetworkEngine.h"
 
 @implementation CSNetworkEngine {
